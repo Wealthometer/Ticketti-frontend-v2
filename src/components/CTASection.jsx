9be00f8f8fab91284c524978057994e2630ed121
Wealthometer <wealthhajoh87@gmail.com>
2026-05-25 16:19:34 +0100

    Create CTASection.jsx

---
+++ src/components/CTASection.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { ArrowRight } from "lucide-react";
+
+export default function CTASection() {
+  return (
+    <section id="cta" className="bg-white px-4 pb-20 sm:px-6 lg:px-8">
