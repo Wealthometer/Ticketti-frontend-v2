6b9b9c47c1f8c41a3299a0689acc74254808a342
Wealthometer <wealthhajoh87@gmail.com>
2026-05-25 16:09:38 +0100

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -5,4 +5,5 @@ import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
