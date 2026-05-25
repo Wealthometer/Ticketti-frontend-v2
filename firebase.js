3d1bdea77bf7c703e81e509f6c6128426c8d8f95
Wealthometer <wealthhajoh87@gmail.com>
2026-05-25 16:09:50 +0100

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -8,3 +8,4 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
+  apiKey: "AIzaSyAb_VOfvEeMLtO-2gsgEaXs1zEQpHnRp8s",
