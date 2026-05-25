845799e1d540b108b50f81aa16e4b9cf3de54b17
Wealthometer <wealthhajoh87@gmail.com>
2026-05-25 16:08:24 +0100

    Update eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -4,3 +4,5 @@ import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
+export default defineConfig([
+  globalIgnores(['dist']),
