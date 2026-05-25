ff63734bcdb781aeddeb3baa626d9de83bf0cdca
Wealthometer <wealthhajoh87@gmail.com>
2026-05-25 16:11:42 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,3 +6,7 @@ import SignIn from "./pages/SignIn.jsx";
 import AdminLogin from "./pages/AdminLogin.jsx";
 import SignUp from "./pages/SignUp.jsx";
 import OTPVerify from "./pages/OTPVerify.jsx";
+import ForgotPassword from "./pages/ForgotPassword.jsx";
+import ResetPassword from "./pages/ResetPassword.jsx";
+
+// Main pages
