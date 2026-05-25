5dfc4cf6a9b875d572da67cf4cd6751570776dba
Wealthometer <wealthhajoh87@gmail.com>
2026-05-25 16:09:34 +0100

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -4,3 +4,4 @@ import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
+const firebaseConfig = {
